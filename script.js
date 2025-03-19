c90d0a964eca3a8c005a4efddf2ecb71365f5603
Lucas <lucas_r_kormann@estudante.sesisenai.org.br>
2025-03-19 10:07:40 -0300

    UPDATE - Atividade 3 - Contagem Regressiva

--- script.js
+++ script.js
@@ -1,25 +1,12 @@
-function operacaoMatematica (numero1, numero2){
+let n1 = 10;
+let delay = 0;
 
-    const operacao1 = numero1 - numero2
-    console.log("Operação 1: " + operacao1);
-
-    const operacao2 = numero1 + numero2
-    console.log("Operação 1: " + operacao2);
-
-    const operacao3 = numero1 * numero2
-    console.log("Operação 1: " + operacao3);
-
-    const operacao4 = numero2!==0 ? numero1 / numero2: "Impossível dividir por 0"
-    console.log("Operação 1: " + operacao4);
-
+while(n1 >= 1) {
+        console.log(n1);
+        n1--
    
+
 }
-
-let numero1 = parseFloat(prompt("Digite o primeiro número:"))
-let numero2 = parseFloat(prompt("Digite o segundo número:"))
-
-operacaoMatematica(numero1,numero2)
-
 
 
 
