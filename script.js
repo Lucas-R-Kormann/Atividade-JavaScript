de805a603fa40dd58a9695bb52346f6f2986863e
Lucas <lucas_r_kormann@estudante.sesisenai.org.br>
2025-03-19 09:59:32 -0300

    UPDATE - Atividade 2 - Operações Matemáticas Simples

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
-numero = prompt("Digite um número");
+function operacaoMatematica (numero1, numero2){
 
-if(numero % 2 === 0) {
-    console.log("O número é par");
-} else {
-    console.log("O número é ímpar");
+    const operacao1 = numero1 - numero2
+    console.log("Operação 1: " + operacao1);
+
+    const operacao2 = numero1 + numero2
+    console.log("Operação 1: " + operacao2);
+
+    const operacao3 = numero1 * numero2
+    console.log("Operação 1: " + operacao3);
+
+    const operacao4 = numero2!==0 ? numero1 / numero2: "Impossível dividir por 0"
+    console.log("Operação 1: " + operacao4);
+
+   
 }
+
+let numero1 = parseFloat(prompt("Digite o primeiro número:"))
+let numero2 = parseFloat(prompt("Digite o segundo número:"))
+
+operacaoMatematica(numero1,numero2)
+
+
+
 
 
 
