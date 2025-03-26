9303154f6ecb44bb9111e1b45d86265e7443e074
Lucas <lucas_r_kormann@estudante.sesisenai.org.br>
2025-03-26 13:39:15 -0300

    UPDATE - Atividade 16 - Array com frutas

--- script.js
+++ script.js
@@ -1,14 +1,6 @@
-function verificarTamanho(string) {
-    if (string.length > 10) {
-      console.log("A string tem mais de 10 caracteres.");
-    } else {
-      console.log("A string tem 10 caracteres ou menos.");
-    }
-  }
-  
-  verificarTamanho("Exemplo curto");  
-  verificarTamanho("Curto");
-  verificarTamanho('String Longa')    
+const frutas = ["Maçã", "Banana", "Laranja", "Uva", "Abacaxi"];
+
+console.log(frutas[2]); 
 
 
 
