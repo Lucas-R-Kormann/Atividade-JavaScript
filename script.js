b6912e6b988095429e9829b5a32d59cc3655a050
Lucas <lucas_r_kormann@estudante.sesisenai.org.br>
2025-03-26 08:30:13 -0300

    UPDATE - Atividade 6 - Modelo do Carro

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-let str = "JavaScript";
-let n = str.length;
-console.log(n)
+class Carro {
+    constructor(marca, modelo, ano) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.ano = ano;
+    }
+}
 
+const carro = new Carro('Nissan', 'Versa', '2022');
 
-
-
+console.log(carro.modelo)
 
