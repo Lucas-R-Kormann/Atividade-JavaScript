f61631993c13d7358902a166adc78ebc68b0f146
Lucas <lucas_r_kormann@estudante.sesisenai.org.br>
2025-03-26 09:25:17 -0300

    UPDATE - Atividade 10 - Verificar Palíndromo

--- script.js
+++ script.js
@@ -1,16 +1,5 @@
-let N1 = parseInt(prompt('Digite o primeiro número'))
-let N2 = parseInt(prompt('Digite o segundo número'))
-let N3 = parseInt(prompt('Digite o terceiro número'))
-
-let soma = N1 + N2 + N3
-
-let media = soma/3
-
-let aprovado = ("Aprovado com a nota: " + media)
-let reprovado = ("Reprovado com a nota: " + media)
-
-if (media >= 7) {
-    console.log(aprovado)
-} else {
-    console.log(reprovado)
+function palindrome(str) {
+    return true;
 }
+
+console.log(palindrome("arara"))
