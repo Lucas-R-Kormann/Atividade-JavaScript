4c5ec8289639ac64e767e2b14101d9e310cafe38
Lucas <lucas_r_kormann@estudante.sesisenai.org.br>
2025-03-26 13:47:11 -0300

    UPDATE - Atividade 20 - Contagem de vogais

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
-for (let i = 0; i <= 30; i++) {
-    if (i % 2 === 0) {
-      console.log(i);
+function contarVogais(palavra) {
+    const vogais = "aeiouAEIOU"; 
+    let contador = 0;
+  
+    for (let letra of palavra) {
+      if (vogais.includes(letra)) {
+        contador++;
+      }
     }
+    return contador;
   }
-
+  console.log(contarVogais("programacao")); 
+  console.log(contarVogais("Javascript")); 
+  console.log(contarVogais("Vogal")); 
 
 
 
