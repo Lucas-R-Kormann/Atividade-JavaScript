4c514baf5f204593f728c5a2890ed67fc7b4059b
Lucas <lucas_r_kormann@estudante.sesisenai.org.br>
2025-03-26 13:52:56 -0300

    UPDATE - Atividade 22 - Função para dobrar números

--- script.js
+++ script.js
@@ -1,14 +1,8 @@
-function somarArray(numeros) {
-    let soma = 0;
-  
-    for (let numero of numeros) {
-      soma += numero;
-    }
-  
-    return soma;
-  }
-  console.log(somarArray([1, 2, 3, 4, 5]));
-  console.log(somarArray([10, 20, 30]));
+let numero = parseInt(prompt('Insira um número e o sistema retornará o dobro deste valor'));
+
+numeroDobrado = (numero * 2);
+
+console.log(numeroDobrado);
   
 
 
