894cbede96210b40a022d5024656f9da02fbfdd0
Lucas <lucas_r_kormann@estudante.sesisenai.org.br>
2025-03-26 09:06:08 -0300

    UPDATE - Atividade 8 - Calculadora de Média

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-let nome = prompt();
+let N1 = parseInt(prompt('Digite o primeiro número'))
+let N2 = parseInt(prompt('Digite o segundo número'))
+let N3 = parseInt(prompt('Digite o terceiro número'))
 
-let mensagem = ('Olá ' + nome);
+let soma = N1 + N2 + N3
 
-console.log(mensagem)
+let media = soma/3
 
+let aprovado = ("Aprovado com a nota: " + media)
+let reprovado = ("Reprovado com a nota: " + media)
+
+if (media >= 7) {
+    console.log(aprovado)
+} else {
+    console.log(reprovado)
+}
